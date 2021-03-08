e3db6b8e0a1765f64ecf6a677c65c641bd6efba7
Rajasri-kolli <54495276+Rajasri-kolli@users.noreply.github.com>
2021-03-08 22:20:21 +0530

    Update main.py

--- project0/main.py
+++ project0/main.py
@@ -1,1 +1,31 @@
 
+# Example main.py
+import argparse
+from project0 import project0
+
+
+def main(url):
+    # Download data
+    data = project0.fetchincidents(url)
+
+    # Extract Data
+    incidents = project0.extractincidents(data)
+
+    # Create Database
+    db = project0.createdb()
+
+    # Insert Data
+    project0.populatedb(db, incidents)
+
+    # Print Status
+    project0.status(db)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--incidents", type=str, required=True,
+                        help="The incidents summary url.")
+
+    args = parser.parse_args()
+    if args.incidents:
+        main(args.incidents)
